880f6c08d492c56f3d4026f4ad4b01a5463236ef
chc4036 <kin.chan24@imperial.ac.uk>
2026-02-03 05:12:54 +0000

    added some

--- data/analysis.py
+++ data/analysis.py
@@ -30,32 +30,3 @@ for cat in categories:
 res = minimize(neg_log_likelihood, x0=[1.0], args=(np.array(all_p), np.array(all_y)), bounds=[(0.01, 10)])
 print(f"MLE theta: {res.x[0]:.4f}")
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-# 1. Define the range for theta (the x-axis of your plot)
-# We look around our MLE (1.0778) to see the peak clearly
-theta_range = np.linspace(0.5, 1.8, 100)
-
-# 2. Calculate log-likelihood for each theta in the range
-log_vals = [neg_log_likelihood(t, all_p_clean, all_y_clean) for t in theta_range]
-
-# 3. Create the plot
-plt.figure(figsize=(8, 5))
-plt.plot(theta_range, log_vals, color='blue', linewidth=2, label='Log-Likelihood $\ell(\\theta)$')
-
-# 4. Mark the Maximum Likelihood Estimate (MLE)
-mle_theta = 1.0778
-max_log = log_likelihood(mle_theta, all_p_clean, all_y_clean)
-plt.scatter(mle_theta, max_log, color='red', zorder=5)
-plt.annotate(f'MLE $\\hat{{\\theta}} = {mle_theta:.4f}$', 
-             xy=(mle_theta, max_log), xytext=(mle_theta+0.1, max_log-5),
-             arrowprops=dict(facecolor='black', shrink=0.05, width=1, headwidth=5))
-
-# 5. Labelling (Essential for the 8-page report)
-plt.title('Log-Likelihood Function for Market Efficiency')
-plt.xlabel('Efficiency Parameter $\\theta$')
-plt.ylabel('Log-Likelihood $\ell(\\theta)$')
-plt.grid(True, linestyle='--', alpha=0.7)
-plt.legend()
-plt.show()
