1026bdb2c07c2c7873950e58af9ed047e5136d97
chc4036 <kin.chan24@imperial.ac.uk>
2026-02-04 05:20:51 +0000

    added more

---
+++ data/Sabrina.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from scipy.optimize import minimize
+
+
+df = pd.read_csv('Sabrina(12-11,25-1).csv')
+Outcome = 'Exactly 0'
+#Cleaning up data
+headers = list(df.columns)
+headers.remove('timestamp')
+headers.sort()
+df = df.dropna(subset=headers)
+#Discard anomalies (greater or less than 3 standard deviations from mean)
+
+for i in headers:
+    mean = df[i].mean()
+    sd = df[i].std()
+    upper = mean + 3*sd
+    lower = mean - 3*sd
+    df = df[(df[i] >= lower) & (df[i] <= upper)]
+
+outcomes = {cat: (1 if cat == Outcome else 0) for cat in headers}
+
+#Transform it into log-likelihood
+df_prices = df[headers] / 100.0
+
+def negLogLikelihood(theta):
+    result = 0
+    for i in headers:
+        p = np.clip(df_prices[i].values,1e-7, 1-1e-7)
+        if i == Outcome:
+            result += np.sum(np.log(p**theta))
+        else:
+            result += np.sum(np.log(1-p**theta))
+    return -result
+
+res = minimize(negLogLikelihood, 1.0)
+
+print(f"MLE theta = {res.x[0]:.4f}")
